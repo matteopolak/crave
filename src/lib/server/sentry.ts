c43e868fdda8b593621a88926c3f8883f288b840
matteopolak <matteopolak@hotmail.com>
2023-12-31 01:57:21 -0500

    fix: build errors

--- src/lib/server/sentry.ts
+++ src/lib/server/sentry.ts
@@ -1,25 +1,27 @@
-import Sentry from '@sentry/node/cjs';
+// import Sentry from '@sentry/node/cjs';
 
-import { SENTRY_DSN } from '$env/static/private';
+// import { SENTRY_DSN } from '$env/static/private';
 
-Sentry.init({
-	dsn: SENTRY_DSN,
-	integrations: [
-		new Sentry.Integrations.Postgres(),
-	],
-	tracesSampleRate: 1.0,
-	profilesSampleRate: 1.0,
-});
+// Sentry.init({
+// 	dsn: SENTRY_DSN,
+// 	integrations: [
+// 		new Sentry.Integrations.Postgres(),
+// 	],
+// 	tracesSampleRate: 1.0,
+// 	profilesSampleRate: 1.0,
+// });
 
 type ToSql = { toSQL: () => { sql: string } };
 
-export function get<T>(input: T & ToSql): Promise<T> {
-	return Sentry.startSpan(
-		{
-			op: 'db.query',
-			name: input.toSQL().sql,
-			data: { 'db.system': 'postgresql' },
-		},
-		async () => input,
-	);
+export async function get<T>(input: T & ToSql): Promise<T> {
+	// return Sentry.startSpan(
+	// 	{
+	// 		op: 'db.query',
+	// 		name: input.toSQL().sql,
+	// 		data: { 'db.system': 'postgresql' },
+	// 	},
+	// 	async () => input,
+	// );
+
+	return input;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
 		sveltekit(),
 		Icons({
 			compiler: 'svelte',
-		})],
+		}),
+	],
 });
